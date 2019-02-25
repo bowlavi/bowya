1352c3f276294fe1a142e76aa1152d8df340811f
bowlavi <47982679+bowlavi@users.noreply.github.com>
2019-02-25 21:36:49 +0530

    Create program1.c

---
+++ program1.c
@@ -0,0 +1,19 @@
+void main()
+{
+int a;
+scanf("%d",a);
+printf("enter the number");
+if(a>0)
+{
+printf("positive");
+}
+else if(a<0)
+{
+printf("negative");
+}
+else(a=0)
+{
+printf("invalid");
+}
+getch();
+}
