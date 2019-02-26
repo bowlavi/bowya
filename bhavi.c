227b8e9f8dbbb4f6d34e8fc1c7c8d74019eb612a
bowlavi <47982679+bowlavi@users.noreply.github.com>
2019-02-26 15:51:44 +0530

    Create bhavi.c

---
+++ bhavi.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+clrscr();
+scanf("%d",&a);
+if(a>0)
+{
+printf("positive");
+}
+else if(a<0)
+{
+printf("negative");
+}
+else
+{
+printf("invalid");
+}
+getch();
+}
