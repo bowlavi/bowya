9617ac7f9d39d0d8cd1d5ed647846f7039d70673
bowlavi <47982679+bowlavi@users.noreply.github.com>
2019-02-28 15:48:59 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+chara;
+clrscr();
+scanf("%c",&a);
+if(a=='A'=='a'||'E'||a=='e'||a=='I'||a=='i'||a=='0'||a=='0'||a=='v'||a=='a')
+{
+printf("vowel");
+}
+else
+printf("constant");
+getch();
+}
