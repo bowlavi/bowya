ceabfa633e2ae00e4f6914999a66e9d65459a043
bowlavi <47982679+bowlavi@users.noreply.github.com>
2019-02-28 16:13:27 +0530

    Create leap.c

---
+++ leap.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+int year;
+clrscr();
+scanf("%d",&year);
+if(%r%4==0)
+{
+if(year%100==0)
+{
+if(year%400==0)
+{
+printf("leap year");
+}
+else
+{
+printf("not a leap year");
+}
+else
+{
+printf("not");
+}
+}
+}
+getch();
+}
