b8ef6468bde8ae157a831f862960e5314e657b62
bowlavi <47982679+bowlavi@users.noreply.github.com>
2019-03-22 10:58:53 +0530

    Create space.c

---
+++ space.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main()
+
+{
+
+    int p,q,num;
+    printf("enter the p,q value");
+    scanf("%d%d",&p,&q);
+    {
+        for(i=p;i<=q,i++)
+        {
+            if(i%2==1)
+            printf("print the i value%d",i);
+        }
+    }
+
+}
