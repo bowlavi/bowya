82d7bb4cfb128fec24d1b82da629b63cab1793be
bowlavi <47982679+bowlavi@users.noreply.github.com>
2019-03-22 11:15:48 +0530

    Create factorialnn.c

---
+++ factorialnn.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int sum=1,j,n;
+    printf("enter the n value");
+    scanf("%d",&n);
+    for(j=1;j<=n;j++)
+    {
+        sum=sum*i;
+    }
+    printf("%d",sum);
+}
