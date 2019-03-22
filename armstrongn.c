0db08e499bd7c7ced5621d8b5d86880b1ea4e554
bowlavi <47982679+bowlavi@users.noreply.github.com>
2019-03-22 11:12:54 +0530

    Create armstrongn.c

---
+++ armstrongn.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main()
+{
+int a,b,n1,temp=0,c,i;
+printf("enter the number");
+scanf("%d%d",&a,&b);
+for(i=a;i<b;i++)
+{
+    c=a;
+while(n>0)
+{
+n1=n%10;
+temp=temp+(n1*n1*n1);
+a=a/10;
+}
+
+
+if(temp==d)
+{
+printf(" armstrong number");
+}
+else
+{
+printf(" not an armstrong");
+}
+}
+}
