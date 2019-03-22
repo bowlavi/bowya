e992787231aa2ffd804a67567a470ed14b0e4a62
bowlavi <47982679+bowlavi@users.noreply.github.com>
2019-03-22 11:38:32 +0530

    Create complex.c

---
+++ complex.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void main()
+{
+int a[10],a,b,temp,n;
+printf("enter the number");
+scanf("%d",&n);
+printf("enter the number");
+for(a=0;a<n;a++)
+{
+scanf("%d",&c[a]);
+}
+for(a=0;a<n;a++)
+{
+for(b=a+1;b<n;b++)
+{
+if(c[a]>c[b])
+{
+temp=a[a];
+c[a]=c[b];
+c[b]=temp;
+}
+}
+}
+for(a=0;a<n;a++)
+{
+printf("%d",c[a]);
+}
+}
+.
