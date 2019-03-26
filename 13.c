f920f774899c628ffd99c182fc1e03bdee4d7703
bowlavi <47982679+bowlavi@users.noreply.github.com>
2019-03-26 15:57:33 +0530

    Create 13.c

---
+++ 13.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+	int l,r,i,n,count=0;
+	scanf("%d %d",&l,&r);
+	for(i=l;i<=r;i++)
+	{
+	   n=i*i;
+	   if(n<=r)
+	   {
+	       count++;
+	   }
+	}
+	printf("%d",count);
+	getch();
+}
